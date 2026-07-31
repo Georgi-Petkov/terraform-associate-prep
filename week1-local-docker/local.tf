67a6d2be5a8236642309c26c6971d05bab60e875
Georgi Petkov <your-actual-email@example.com>
2026-07-31 21:17:53 +0200

    Day 2: add local_file with explicit depends_on

--- week1-local-docker/local.tf
+++ week1-local-docker/local.tf
@@ -9,3 +9,13 @@ resource "local_file" "greeting" {
     Tags: ${jsonencode(var.greeting_tags)}
   EOT
 }
+resource "local_file" "collaborate" {
+  filename = "${path.module}/output/collaborate.txt"
+  content  = <<-EOT
+  Terraform allows you to collaborate on your infrastructure with its remote state backends. When you use HCP Terraform (free for up to five users), you can securely share your state with your teammates, provide a stable environment for Terraform to run in, and prevent race conditions when multiple people make configuration changes at once.
+
+You can also connect HCP Terraform to version control systems (VCSs) like GitHub, GitLab, and others, allowing it to automatically propose infrastructure changes when you commit configuration changes to VCS. This lets you manage changes to your infrastructure through version control, as you would with application code.
+EOT
+
+  depends_on = [docker_container.web]
+}
